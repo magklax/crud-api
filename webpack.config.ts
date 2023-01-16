f6943cec7a13cb4a5cac4590800b0e7cf93b411f
Nadezda <nadezda.prokopyeva@brytlyt.com>
2023-01-16 17:02:50 +0100

    fix: support crypto

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,21 @@
 import * as path from 'path';
 import { Configuration } from 'webpack';
+import nodeExternals from "webpack-node-externals";
 
 const config: Configuration = {
   mode: 'production',
-  externalsPresets: { node: true },
   entry: './src/index.ts',
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+  },
   output: {
-    // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
   },
   resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+    extensions: ['.ts'],
   },
   optimization: {
     minimize: false,
